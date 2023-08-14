8eb056e0901fe5dd5c2c202bc08444f5655396fa
RicoBasyar <RicoBasyar@users.noreply.github.com>
2023-08-15 05:17:06 +0700

    Second Commit

--- Rico_Discord.py
+++ Rico_Discord.py
@@ -3,7 +3,7 @@ from Rico_Selenium import open_search_tokped
 import asyncio
 import discord
  
-token = 'NTMyNDU0MjgwOTgwODU2ODMz.G3piyn.W_n7TKamdru1WaTRfvdmR0Ahl7en7FuVse5dlw'
+token = '#'
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
